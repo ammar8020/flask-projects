1975435b692477cecb8f72097db80512e3dd5f67
ammary7 <ammaryahmed7@gmail.com>
2019-11-06 13:29:18 +0500

    section3 added basic templates

--- projects/project1/routes.py
+++ projects/project1/routes.py
@@ -1,24 +1,86 @@
 from project1 import app
-from flask import render_template
+from flask import render_template, request, json, Response
+
+movies_data = [
+    {
+        "movieID": "1111",
+        "title": "PHP 111",
+        "description": "Intro to PHP",
+        "credits": "3",
+        "term": "Fall, Spring",
+    },
+    {
+        "movieID": "2222",
+        "title": "Java 1",
+        "description": "Intro to Java Programming",
+        "credits": "4",
+        "term": "Spring",
+    },
+    {
+        "movieID": "3333",
+        "title": "Adv PHP 201",
+        "description": "Advanced PHP Programming",
+        "credits": "3",
+        "term": "Fall",
+    },
+    {
+        "movieID": "4444",
+        "title": "Angular 1",
+        "description": "Intro to Angular",
+        "credits": "3",
+        "term": "Fall, Spring",
+    },
+    {
+        "movieID": "5555",
+        "title": "Java 2",
+        "description": "Advanced Java Programming",
+        "credits": "4",
+        "term": "Fall",
+    },
+]
 
 
 @app.route("/")
 @app.route("/index")
 @app.route("/home")
 def index():
-    return render_template("index.html", login=False)
+    return render_template("index.html", index=True)
 
 
 @app.route("/login")
 def login():
-    return render_template("login.html", login=False)
+    return render_template("login.html", login=True)
 
 
-@app.route("/movies")
-def movies():
-    return render_template("movies.html", login=False)
+@app.route("/movies/")
+@app.route("/movies/<term>")
+def movies(term="Spring 2019"):
+    return render_template(
+        "movies.html", movies_data=movies_data, movies=True, term=term
+    )
 
 
 @app.route("/register")
 def register():
-    return render_template("register.html", login=False)
+    return render_template("register.html", register=True)
+
+
+@app.route("/watchlist", methods=["GET", "POST"])
+def watchlist():
+    id = request.form.get("movieID")
+    title = request.form.get("title")
+    term = request.form.get("term")
+    return render_template(
+        "watchlist.html", watchlist=True, movie={"id": id, "title": title, "term": term}
+    )
+
+
+@app.route("/api/")
+@app.route("/api/<id>")
+def api(id=None):
+    if id == None:
+        json_data = movies_data
+    else:
+        json_data = movies_data[int(id)]
+
+    return Response(json.dumps(json_data), mimetype="application/json")
